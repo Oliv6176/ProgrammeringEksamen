4e0742492e52cc826817e3e21becfee2a7b6278b
Oliver Geneser <32340667+Oliv6176@users.noreply.github.com>
2020-05-05 11:53:36 +0200

    2.1

--- board.php
+++ board.php
@@ -28,7 +28,7 @@
             $allowTypes = array('jpg','png','jpeg','gif'); 
 
             if( in_array($fileType, $allowTypes )){
-                $targetFilePath = "../programmering/ProgrammeringEksamen/images/";
+                $targetFilePath = "images/";
 
                 $temp = explode(".", $fileName);
                 $newfilename = uniqid (rand (), true).'.'.end($temp);
